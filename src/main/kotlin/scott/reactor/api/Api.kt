5a722b9b6c34e56d9a80e81846925ac9f87e973f
exssinclair <scott.sinclair@extern.wrwks.at>
2021-10-27 09:59:48 +0200

    api is normal classes rather than extension functions which have to be individually imported

--- src/main/kotlin/scott/reactor/api/Api.kt
+++ src/main/kotlin/scott/reactor/api/Api.kt
@@ -1,87 +1,96 @@
 package scott.reactor.api
 
 import org.reactivestreams.Publisher
-import org.reactivestreams.Subscriber
 import scott.reactor.core.CorePublisher
 import scott.reactor.operations.*
 
-
 /**
-Monos and Fluxes don't really exist in terms of separate implementations, we just have Publishers
-So we only present Mono and Flux in the API for technical contract purposes (Mono<T> is contracted to asynchronously emit only 1 event)
+ * Fluxes and Monos are not technically required, you can do everything at the  Publisher level
+ * But they provide nice types for an API so a Mono<String> will clearly return just one value....
+ *
+ * So we just create the API by wrapping publishers in Flux or Mono wrappers
+ * It allows us to have the same implementations for both, as usually the code which works for emitting
+ * N events is also correct for emitting only 1 event.
  */
-interface Flux<T> : Publisher<T>
-interface Mono<T> : Publisher<T>
 
-class FluxImpl<T>(val wrapping: Publisher<T>) : Flux<T> {
-    override fun subscribe(subscriber: Subscriber<in T>?) {
-        wrapping.subscribe(subscriber)
-    }
+
+open class Flux<T>(val wrapping: Publisher<T>) : Publisher<T> by wrapping {
+    /**
+     * buffer can be used to buffer a single event or multiple events
+     */
+    fun buffer() = BufferedPublisher(this).presentAsFlux()
+
+    /**
+     * Collecting a list must be from a Flux (many) and returns a Mono (single result of the whole list)
+     */
+    fun collectList() = CollectPublisher(this).presentAsMono()
+
+    /**
+     * Fliltering a Flux returns a Flux
+     */
+    fun filter(predicate: (T) -> Boolean) = FilteredPublisher(predicate, this).presentAsFlux()
+
+    /**
+     * Mapper
+     */
+    fun <DEST> map(mapper: (T) -> DEST) = MappedPublisher(mapper, this).presentAsFlux()
+
+
+    /**
+     * Flatmap for 1 event emitting N events, so both Mono and Flux can  this, the output in both cases is a Flux
+     */
+    fun <DEST> flatMap(mapper: (T) -> Publisher<DEST>): Publisher<DEST> =
+        FlatMapPublisher(mapper, this).presentAsFlux()
+
+    /**
+     * Only Flux can have a next
+     */
+    fun next() = NextPublisher(this).presentAsMono()
+
+
 }
 
-class MonoImpl<T>(val wrapping: Publisher<T>) : Mono<T> {
-    override fun subscribe(subscriber: Subscriber<in T>?) {
-        wrapping.subscribe(subscriber)
-    }
+open class Mono<T>(val wrapping: Publisher<T>) : Publisher<T> by wrapping{
+
+    /**
+     * buffer can be used to buffer a single event or multiple events
+     */
+    fun buffer() = BufferedPublisher(this).presentAsMono()
+
+    /**
+     * A filtered Mono, when the predicate is false, it will just complete without emitting anything
+     */
+    fun filter(predicate: (T) -> Boolean) = FilteredPublisher(predicate, this).presentAsMono()
+
+    /**
+     * Mapper
+     */
+    fun <DEST> map(mapper: (T) -> DEST) = MappedPublisher(mapper, this).presentAsMono()
+
+    /**
+     * Flatmap for 1 event emitting N events, so both Mono and Flux can  this, the output in both cases is a Flux
+     */
+    fun <DEST> flatMap(mapper: (T) -> Publisher<DEST>): Publisher<DEST> =
+        FlatMapPublisher(mapper, this).presentAsFlux()
+
 }
 
-fun <T> Publisher<T>.presentAsMono() : Mono<T> = MonoImpl(this)
-fun <T> Publisher<T>.presentAsFlux() : Flux<T> = FluxImpl(this)
-
-/**
- * buffer can be used to buffer a single event or multiple events
- */
-fun <T> Mono<T>.buffer() = BufferedPublisher(this).presentAsMono()
-fun <T> Flux<T>.buffer() = BufferedPublisher(this).presentAsFlux()
-
-/**
- * Collecting a list must be from a Flux (many) and returns a Mono (single result of the whole list)
- */
-fun <T> Flux<T>.collectList() = CollectPublisher(this).presentAsMono()
+fun <T> Publisher<T>.presentAsMono() : Mono<T> = Mono(this)
+fun <T> Publisher<T>.presentAsFlux() : Flux<T> = Flux(this)
 
 /**
  * Concat can equally concat Monos and Fluxes and returns a Flux
  */
 fun <T> List<Publisher<T>>.concat() = ConcatPublisher(this).presentAsFlux()
 
-/**
- * Fliltering a Flux returns a Flux
- */
-fun <T> Flux<T>.filter(predicate: (T) -> Boolean) = FilteredPublisher(predicate, this).presentAsFlux()
-
-/**
- * A filtered Mono, when the predicate is false, it will just complete without emitting anything
- */
-fun <T> Mono<T>.filter(predicate: (T) -> Boolean) = FilteredPublisher(predicate, this).presentAsMono()
 
 
-/**
- * Flatmap for 1 event emitting N events, so both Mono and Flux can  this, the output in both cases is a Flux
- */
-
-fun <T, DEST> Publisher<T>.flatMap(mapper: (T) -> Publisher<DEST>): Publisher<DEST> =
-    FlatMapPublisher(mapper, this).presentAsFlux()
-
-/*
-* Flux and Mono mappers
-*/
-fun <SOURCE, DEST> Flux<SOURCE>.map(mapper: (SOURCE) -> DEST) = MappedPublisher(mapper, this).presentAsFlux()
-fun <SOURCE, DEST> Mono<SOURCE>.map(mapper: (SOURCE) -> DEST) = MappedPublisher(mapper, this).presentAsMono()
-
-
-/**
- * Only Flux can have a next
- */
-fun <T> Flux<T>.next() = NextPublisher(this).presentAsMono()
-
-class FluxSink<T>(val corePublisher: CorePublisher<T>) : Flux<T> {
-    override fun subscribe(subscriber: Subscriber<in T>) = corePublisher.subscribe(subscriber)
+class FluxSink<T>(val corePublisher: CorePublisher<T>) : Flux<T>(corePublisher){
     fun emitNext(vararg events: T) = corePublisher.emitNext(*events)
     fun complete() = corePublisher.complete()
 }
 
-class MonoSink<T>(val corePublisher: CorePublisher<T>) : Mono<T> {
-    override fun subscribe(subscriber: Subscriber<in T>) = corePublisher.subscribe(subscriber)
+class MonoSink<T>(val corePublisher: CorePublisher<T>) : Mono<T>(corePublisher) {
     fun emitNext(vararg events: T) = corePublisher.emitNext(*events)
     fun complete() = corePublisher.complete()
 }
